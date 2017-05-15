6d7113c19dd0d02129567bb9a21b588ecb7b5246
Jolly Walia <jwalia@hammer.local>
2017-05-15 15:12:57 -0700

    Added store database.

---
+++ store_db.sql
@@ -0,0 +1,22 @@
+CREATE TABLE store (id INTEGER PRIMARY KEY, item TEXT, size TEXT, volume INTEGER, price NUMERIC);
+
+INSERT INTO store VALUES (1, "Orange", "small", 12, 5.50);
+INSERT INTO store VALUES (2, "Orange", "medium", 16,  6.50);
+INSERT INTO store VALUES (3, "Orange", "large",  24, 7.50);
+
+INSERT INTO store VALUES (4, "Apple", "small", 12, 5.50);
+INSERT INTO store VALUES (5, "Apple", "medium", 16,  6.50);
+INSERT INTO store VALUES (6, "Apple", "large",  24, 7.50);
+
+INSERT INTO store VALUES (8, "Carrot", "small", 12, 7.50);
+INSERT INTO store VALUES (9, "Carrot", "medium", 16, 8.50);
+INSERT INTO store VALUES (10, "Carrot", "large",  24, 9.50);
+
+INSERT INTO store VALUES (11, "Mango", "small", 12, 7.50);
+INSERT INTO store VALUES (12, "Mango", "medium", 16, 8.50);
+INSERT INTO store VALUES (13, "Mango", "large",  24, 9.50);
+
+INSERT INTO store VALUES (14, "Pineapple", "small", 12, 7.50);
+INSERT INTO store VALUES (15, "Pineapple", "medium", 16, 8.50);
+INSERT INTO store VALUES (16, "Pineapple", "large",  24, 9.50);
+
