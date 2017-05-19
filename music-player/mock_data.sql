ee7c420c54b02b9ce2c8622b817fb112247e0df9
Jolly Walia <jwalia@hammer.local>
2017-05-19 10:47:11 -0700

    Added INSERT.

--- music-player/mock_data.sql
+++ music-player/mock_data.sql
@@ -1,29 +1,25 @@
-albums (id, artist_id, name)
-1	2	Chicago 19
-2	1	Master of Puppets
-3	3	Thriller
+INSERT INTO albums VALUES (1,2,'Chicago 19');
+INSERT INTO albums VALUES (2,1,'Master of Puppets');
+INSERT INTO albums VALUES (3,3,'Thriller');
 
-artists (id, name, albums, genre)
-1	Metallica	10	Metal 
-2	Chicago	36	Soft Rock
-3	Michael Jackson	10	Pop
+INSERT INTO artists VALUES (1,'Metallica,10,Metal' );
+INSERT INTO artists VALUES (2,'Chicago,36,Soft Rock');
+INSERT INTO artists VALUES (3,'Michael Jackson,10,Pop');
 
-playlists (id, song_id, name)
-1	2	Jamz
-2	7	Jamz
-3	5	Chill
-4	9	Chill
-5	1	The Feelz
-6	4	The Feelz
-7	8	The Feelz
+INSERT INTO playlists VALUES (1,2,'Jamz');
+INSERT INTO playlists VALUES (2,7,'Jamz');
+INSERT INTO playlists VALUES (3,5,'Chill');
+INSERT INTO playlists VALUES (4,9,'Chill');
+INSERT INTO playlists VALUES (5,1,'The Feelz');
+INSERT INTO playlists VALUES (6,4,'The Feelz');
+INSERT INTO playlists VALUES (7,8,'The Feelz');
 
-songs (id, artist_id, album_id, name, length)
-1	2	1	I Don't Wanna Live Without Your Love	4
-2	3	3	Thriller	5
-3	1	2	Battery	6
-4	2	1	Look Away	5
-5	3	3	Billie Jean	4
-6	1	2	Master of Puppets	3
-7	3	3	Beat It	7
-8	2	1	What Kind of Man Would I Be?	5
-9	1	2	Welcome Home (Sanitarium)	6
+INSERT INTO songs VALUES (1,2,1,'I Dont Wanna Live Without Your Love',4);
+INSERT INTO songs VALUES (2,3,3,'Thriller',5);
+INSERT INTO songs VALUES (3,1,2,'Battery',6);
+INSERT INTO songs VALUES (4,2,1,'Look Away',5);
+INSERT INTO songs VALUES (5,3,3,'Billie Jean',4);
+INSERT INTO songs VALUES (6,1,2,'Master of Puppets',3);
+INSERT INTO songs VALUES (7,3,3,'Beat It',7);
+INSERT INTO songs VALUES (8,2,1,'What Kind of Man Would I Be?',5);
+INSERT INTO songs VALUES (9,1,2,'Welcome Home (Sanitarium);',6);
